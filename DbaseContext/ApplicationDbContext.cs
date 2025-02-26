91d051e425b419b1e5241a3a71949a43bcc5b4c2
KeilynNavarro <127805672+KeilynNavarro@users.noreply.github.com>
2025-02-26 00:57:02 -0600

    CRUD TAREAS

--- DbaseContext/ApplicationDbContext.cs
+++ DbaseContext/ApplicationDbContext.cs
@@ -16,6 +16,9 @@ namespace JarredsOrderHub.DbaseContext
         public DbSet<Cliente> Clientes { get; set; }
         public DbSet<Categoria> Categorias { get; set; }
         public DbSet<Platillo> Platillos { get; set; }
+        public DbSet<Horario> Horario { get; set; }
+        public DbSet<Tareas> Tareas { get; set; }
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -26,6 +29,22 @@ namespace JarredsOrderHub.DbaseContext
                 .WithMany(c => c.Platillos)
                 .HasForeignKey(p => p.IdCategoria)
                 .IsRequired(false);
+
+            modelBuilder.Entity<Horario>().HasData(
+                new Horario { IdHorario = 1, HoraInicio = TimeSpan.Parse("08:00:00"), HoraFin = TimeSpan.Parse("17:00:00") },
+                new Horario { IdHorario = 2, HoraInicio = TimeSpan.Parse("09:00:00"), HoraFin = TimeSpan.Parse("18:00:00") },
+                new Horario { IdHorario = 3, HoraInicio = TimeSpan.Parse("10:00:00"), HoraFin = TimeSpan.Parse("19:00:00") }
+            );
+
+            modelBuilder.Entity<Rol>().HasData(
+                new Rol { Id = 1, Nombre = "Administrador", Descripcion = "Acceso total a la pagina.", Permisos = "Administrar usuarios, Ver empleados, Ver tareas, Administrar tareas" }
+            );
+
+            modelBuilder.Entity<Tareas>()
+            .HasOne(t => t.Empleado)
+            .WithMany()
+            .HasForeignKey(t => t.IdEmpleado)
+            .OnDelete(DeleteBehavior.Cascade);
         }
     }
 }
